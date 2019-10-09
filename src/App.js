d2a870e8c407ab3ab9a7187e2b60ad8365fc6f4f
Donghyun Lee <dominic2009@snu.ac.kr>
2019-10-09 17:06:11 +0900

    Fix error: Add path of DocumentDetail

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App(props) {
           <Route path='/sign_up' exact component={SignUp} />
           <Route path='/tell_me' exact component={TellMe} />
           <Route path='/tell_me/search_fail' exact component={DocumentSearchFail} />
+          <Route path='/tell_me/:document_title' exact component={DocumentDetail} />
           <Route path='/tell_me/create' exact component={DocumentCreate} />
           <Route render={() => <h1>Not Found</h1>} />
         </Switch>
