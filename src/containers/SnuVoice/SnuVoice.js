23cf16d7ba5311e0b9d51b739284fc6895d9c690
Donghyun Lee <dominic2009@snu.ac.kr>
2019-10-14 17:52:44 +0900

    Minor changes in SnuVoice.js

--- src/containers/SnuVoice/SnuVoice.js
+++ src/containers/SnuVoice/SnuVoice.js
@@ -2,43 +2,41 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
-
-import { Button,Input } from 'reactstrap';
+import { Button, Input } from 'reactstrap';
 
 class SnuVoice extends Component {
     state = {
         email: '',
         password: '',
-        sign_in: false
+        sign_in: false,
     }
 
     onClickSignUpButton = () => {
-        this.props.history.push('/sign_up')
+        this.props.history.push('/sign_up');
     }
 
     onClickSignInButton = () => {
-        this.setState({ sign_in: true })
+        this.setState({ sign_in: true });
     }
 
     onClickSignOutButton = () => {
-        this.setState({ sign_in: false })
+        this.setState({ sign_in: false });
     }
 
     onClickTellMeButton = () => {
-        this.props.history.push('/tell_me')
+        this.props.history.push('/tell_me');
     }
 
     onClickHearUsButton = () => {
-        this.props.history.push('/hear_us')
+        this.props.history.push('/hear_us');
     }
-
 
     render() {
         let upper_bar = null;
         if (this.state.sign_in === false) {
             upper_bar = (
                 <div className="UpperBar">
-                    <Input type="email" id="email_input" placeholder="EMAIL"
+                    <Input type="email" id="email_input" placeholder="SNU MAIL"
                         onChange={(event) => this.setState({ email: event.target.value })}></Input>
                     <Input type="password" id="password_input" placeholder="PASSWORD"
                         onChange={(event) => this.setState({ password: event.target.value })}></Input>
@@ -48,16 +46,13 @@ class SnuVoice extends Component {
                         onClick={this.onClickSignUpButton}>SIGN-UP</Button>
                 </div >
             );
-
         }
-        else {
+        else if (this.state.sign_in === true) {
             upper_bar =
                 <div className="UpperBar">
                     <Button type="button" id="sign_out_button"
                         onClick={this.onClickSignOutButton}>SIGN-OUT</Button>
                 </div>
-
-
         }
 
         return (
@@ -70,8 +65,7 @@ class SnuVoice extends Component {
                         onClick={this.onClickHearUsButton}>HEAR-US</Button>
                 </div>
             </div>
-        )
-
+        );
     }
 }
 
