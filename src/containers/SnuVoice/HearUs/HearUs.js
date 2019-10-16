6c9cbb7dba103f63575a94488f147f6f4d622e5a
dkwanm1 <dkwanm00@gmail.com>
2019-10-16 21:52:38 +0900

    Add UpperBar

--- src/containers/SnuVoice/HearUs/HearUs.js
+++ src/containers/SnuVoice/HearUs/HearUs.js
@@ -3,10 +3,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 
+import UpperBar from '../UpperBar/UpperBar'
+
 class HearUs extends Component {
     render() {
         return (
             <div className="HearUs">
+                <UpperBar />
                 <h1>HearUs</h1>
             </div>
         )
