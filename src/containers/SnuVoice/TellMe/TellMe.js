4f09a67f362c075276a84f0bac5b74e15c19e967
dkwanm1 <dkwanm00@gmail.com>
2019-10-16 21:53:07 +0900

    Add UpperBar

--- src/containers/SnuVoice/TellMe/TellMe.js
+++ src/containers/SnuVoice/TellMe/TellMe.js
@@ -3,10 +3,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 
+import UpperBar from '../UpperBar/UpperBar'
+
 class TellMe extends Component {
     render() {
         return (
             <div className="TellMe">
+                <UpperBar />
                 <h1>TellMe</h1>
             </div>
         )
