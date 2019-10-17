488743338ee3b8ea478c9e6197876dcfc047e494
dkwanm1 <dkwanm00@gmail.com>
2019-10-18 01:39:48 +0900

    Add onChangeSearchInput function at TellMe.js

--- src/containers/SnuVoice/TellMe/TellMe.js
+++ src/containers/SnuVoice/TellMe/TellMe.js
@@ -2,18 +2,40 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
+import { Button, Input } from 'reactstrap';
 
 import UpperBar from '../UpperBar/UpperBar'
 
 class TellMe extends Component {
+    state = {
+        search: ''
+    }
+
+    onChangeSearchInput  = (event) => {
+        this.setState({search : event.target.value});
+    }
+
+    onClickSearchConfirmButton = () => {
+        this.props.history.push('/') //리다이렉트 백엔드 구햔하고 해야할듯
+    }
+
+    onClickCreateButton = () => {
+        this.props.history.push('/tell_me/create');
+    }
     render() {
         return (
             <div className="TellMe">
                 <UpperBar />
                 <h1>TellMe</h1>
+                <Input type="text" id="search_input" autoFocus
+                    onChange={this.onChangeSearchInput}></Input>
+                <Button type="button" id="search_confirm_button"
+                    onClick={this.onClickSearchConfirmButton}>Search</Button>
+                <Button type="button" id="create_button"
+                    onClick={this.onClickCreateButton}>Create</Button>
             </div>
-        )
-    }
-}
-
+                );
+            }
+        }
+        
 export default connect(null, null)(withRouter(TellMe));
