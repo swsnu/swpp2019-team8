8c5d5452fc0cc1d367c36ccdde13a6c8cee8c80f
Donghyun Lee <dominic2009@snu.ac.kr>
2019-10-29 17:30:14 +0900

    Implement reducer layout

---
+++ src/store/reducers/tellme.js
@@ -0,0 +1,37 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+    // TODO
+};
+
+const tellmeReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.POST_DOCUMENT:
+            return state;   // TODO
+        case actionTypes.GET_DOCUMENT:
+            return state;   // TODO
+        case actionTypes.PUT_DOCUMENT:
+            return state;   // TODO
+        case actionTypes.POST_PHOTO:
+            return state;   // TODO
+        case actionTypes.GET_PHOTO:
+            return state;   // TODO
+        case actionTypes.PUT_PHOTO:
+            return state;   // TODO
+        case actionTypes.GET_DEBATES:
+            return state;   // TODO
+        case actionTypes.POST_DEBATE:
+            return state;   // TODO
+        case actionTypes.GET_DEBATE:
+            return state;   // TODO
+        case actionTypes.GET_DEBATE_COMMENTS:
+            return state;   // TODO
+        case actionTypes.POST_DEBATE_COMMENT:
+            return state;   // TODO
+        default:
+            break;
+    }
+    return state;
+};
+
+export default tellmeReducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,17 @@
-
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
 import { createBrowserHistory } from 'history';
 
+import userReducer from './reducers/user';
+import tellmeReducer from './reducers/tellme';
+import hearusReducer from './reducers/hearus';
+
 export const history = createBrowserHistory();
 const rootReducer = combineReducers({
+    usr: userReducer,
+    tm: tellmeReducer,
+    hu: hearusReducer,
     router: connectRouter(history),
 });
 export const middlewares = [thunk, routerMiddleware(history)]
