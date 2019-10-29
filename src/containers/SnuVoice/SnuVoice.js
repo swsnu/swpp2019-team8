fd2536ee40afe5a18ede932b92e4d885df98138d
rocky <m.seo1999@gmail.com>
2019-10-29 17:49:55 +0900

    Added img folder in src. Added new button images in main folder

--- src/containers/SnuVoice/SnuVoice.js
+++ src/containers/SnuVoice/SnuVoice.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
-import { Button } from 'reactstrap';
+
+import { Row, Col, Button, Image } from 'react-bootstrap';
 
 import UpperBar from './UpperBar/UpperBar'
+
+import TellMeButton from "../../img/tell_me.png";
+import HearUsButton from "../../img/hear_us.png";
 
 class SnuVoice extends Component {
     state = {
@@ -23,12 +27,18 @@ class SnuVoice extends Component {
         return (
             <div className="SnuVoice">
                 <UpperBar />
-                <div className="Row">
-                    <Button type="button" id="tell_me_button"
-                        onClick={this.onClickTellMeButton}>TELL-ME</Button>
-                    <Button type="button" id="hear_us_button"
-                        onClick={this.onClickHearUsButton}>HEAR-US</Button>
-                </div>
+                <Row>
+                    <Col>
+                        <Button type="button" id="tell_me_button" onClick={this.onClickTellMeButton}>
+                            <Image src={TellMeButton} alt="Go to Tell Me"rounded/>
+                        </Button>
+                    </Col>
+                    <Col>
+                        <Button type="button" id="hear_us_button" onClick={this.onClickHearUsButton}>
+                            <Image src={HearUsButton} alt="Go to Hear Us" rounded/>
+                        </Button>
+                    </Col>
+                </Row>
             </div>
         );
     }
