210c07932697d6f3e1023472e34f9d3cfdf2ae30
dkwanm1 <dkwanm00@gmail.com>
2019-10-29 20:33:55 +0900

    Add USER model

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .forms import UserChangeForm, UserCreationForm
+from .models import User
 
 # Register your models here.
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'nickname', 'gender', 'status',
+                    'studentId', 'department', 'major', 'studentStatus', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('nickname', 'gender', 'status',
+                                      'studentId', 'department', 'major', 'studentStatus')}),
+        ('Permissons', {'fields': ('is_admin',)})
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password', 'nickname', 'gender', 'status',
+                       'studentId', 'department', 'major', 'studentStatus')
+        })
+    )
+
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
