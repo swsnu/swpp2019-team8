2ebae34f603107ffa1344e766bc5079fe5b635fb
rocky <m.seo1999@gmail.com>
2019-10-30 17:05:17 +0900

    Created skeletal code for debate backend view.py

--- backend/tellme/views.py
+++ backend/tellme/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse, HttpResponseForbidden, HttpResponseBadRequest
+
+import json
+
+from .models import Debate, DebateComment
+
 
 # Create your views here.
+
+def debates_by_document(request, document_id):
+    if request.method=='GET':
+        #TODO
+        return HttpResponse(status=200)
+    elif request.method=='POST':
+        #TODO
+        return HttpResponse(status=201)
+    else:
+        return HttpResponseNotAllowed(['GET', 'POST'])
+
+def debate_get(request, document_id, debate_id):
+    if request.method=='GET':
+        #TODO
+        return HttpResponse(status=200)
+    else:
+        return HttpResponseNotAllowed(['GET'])
+
+def debate_comments(request, debate_id):
+    if request.method=='GET':
+        #TODO
+        return HttpResponse(status=200)
+    elif request.method=='POST':
+        #TODO
+        return HttpResponse(status=201)
+    else:
+        return HttpResponseNotAllowed(['GET', 'POST'])
