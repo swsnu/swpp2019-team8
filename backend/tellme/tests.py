f222c3e2ffd1eca9ac1542f3d9ddd37b589f536e
rocky <m.seo1999@gmail.com>
2019-11-02 02:20:09 +0900

    Debate backend testing structure code complete

--- backend/tellme/tests.py
+++ backend/tellme/tests.py
@@ -8,7 +8,28 @@ from user.models import User
 # Create your tests here.
 class DebateTestCase(TestCase):
     def test_debates_by_document(self):
+        client = Client()
+        User.objects.create_user(username='swpp', password='iluvswpp')
+
+        response = client.get()
+        self.assertEqual(response.status_code,)
+
+        response = client.post()
+        self.assertEqual(response.status_code,)
 
     def test_debate_get(self):
+        client = Client()
+        User.objects.create_user(username='swpp', password='iluvswpp')
+
+        response = client.get()
+        self.assertEqual(response.status_code,)
 
     def test_debate_comments(self):
+        client = Client()
+        User.objects.create_user(username='swpp', password='iluvswpp')
+
+        response = client.get()
+        self.assertEqual(response.status_code,)
+        
+        response = client.post()
+        self.assertEqual(response.status_code,)
