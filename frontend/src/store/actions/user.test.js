547914b36aa3627c8cd32199fb45c6115c9650da
dkwanm1 <dkwanm00@gmail.com>
2019-11-01 19:01:07 +0900

    minor changes at uesr.test.js

--- frontend/src/store/actions/user.test.js
+++ frontend/src/store/actions/user.test.js
@@ -18,6 +18,19 @@ describe('Axios User test', () => {
         verifyCode: ''
     }
 
+    let spyGetUser;
+    beforeEach(() => {
+        spyGetUser = jest.spyOn(axios, 'get')
+            .mockImplementation(url => {
+                return new Promise((resolve, reject) => {
+                    const result = {
+                        status: 200,
+                        data: stubUser
+                    };
+                    resolve(result);
+                });
+            })
+    })
     afterEach(() => { jest.clearAllMocks() })
 
     it('signUp should done correctly', (done) => {
@@ -80,85 +93,46 @@ describe('Axios User test', () => {
 
 
     it('getUser by email should fetch User correctly', (done) => {
-        const spy = jest.spyOn(axios, 'get')
-            .mockImplementation(url => {
-                return new Promise((resolve, reject) => {
-                    const result = {
-                        status: 200,
-                        data: stubUser
-                    };
-                    resolve(result);
-                });
-            })
+
 
         store.dispatch(actionCreators.getUserByEmail(stubUser.email))
             .then(() => {
                 const newState = store.getState();
                 expect(newState.usr.selectedUser).toBe(stubUser.selectedUser);
-                expect(spy).toHaveBeenCalledTimes(1);
+                expect(spyGetUser).toHaveBeenCalledTimes(1);
                 done();
             })
     });
 
     it('getUser by id should fetch User correctly', (done) => {
-        const spy = jest.spyOn(axios, 'get')
-            .mockImplementation(url => {
-                return new Promise((resolve, reject) => {
-                    const result = {
-                        status: 200,
-                        data: stubUser
-                    };
-                    resolve(result);
-                });
-            })
 
         store.dispatch(actionCreators.getUserByUserId(stubUser.selectedUser.id))
             .then(() => {
                 const newState = store.getState();
                 expect(newState.usr.selectedUser).toBe(stubUser.selectedUser);
-                expect(spy).toHaveBeenCalledTimes(1);
+                expect(spyGetUser).toHaveBeenCalledTimes(1);
                 done();
             })
     });
 
     it('getUser by studentId should fetch User correctly', (done) => {
-        const spy = jest.spyOn(axios, 'get')
-            .mockImplementation(url => {
-                return new Promise((resolve, reject) => {
-                    const result = {
-                        status: 200,
-                        data: stubUser
-                    };
-                    resolve(result);
-                });
-            })
 
         store.dispatch(actionCreators.getUserByStudentId(stubUser.selectedUser.studentId))
             .then(() => {
                 const newState = store.getState();
                 expect(newState.usr.selectedUser).toBe(stubUser.selectedUser);
-                expect(spy).toHaveBeenCalledTimes(1);
+                expect(spyGetUser).toHaveBeenCalledTimes(1);
                 done();
             })
     });
 
     it('getUser by nickname should fetch User correctly', (done) => {
-        const spy = jest.spyOn(axios, 'get')
-            .mockImplementation(url => {
-                return new Promise((resolve, reject) => {
-                    const result = {
-                        status: 200,
-                        data: stubUser
-                    };
-                    resolve(result);
-                });
-            })
 
         store.dispatch(actionCreators.getUserByNickname(stubUser.selectedUser.nickname))
             .then(() => {
                 const newState = store.getState();
                 expect(newState.usr.selectedUser).toBe(stubUser.selectedUser);
-                expect(spy).toHaveBeenCalledTimes(1);
+                expect(spyGetUser).toHaveBeenCalledTimes(1);
                 done();
             })
     });
