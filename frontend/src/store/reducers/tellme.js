2bbeec7f1b45fbcdea6846ab6411aee058352206
Donghyun Lee <dominic2009@snu.ac.kr>
2019-11-01 19:19:44 +0900

    Add annotations in reducers/tellme.js

--- frontend/src/store/reducers/tellme.js
+++ frontend/src/store/reducers/tellme.js
@@ -7,28 +7,28 @@ const initialState = {
 
 const tellmeReducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.POST_DOCUMENT:
-            return state;   // TODO
+        // case actionTypes.POST_DOCUMENT:
+        //     return state;   // TODO
         case actionTypes.GET_DOCUMENT:
             return { ...state, selectedDocument: action.target };
-        case actionTypes.PUT_DOCUMENT:
-            return state;   // TODO
-        case actionTypes.POST_PHOTO:
-            return state;   // TODO
-        case actionTypes.GET_PHOTO:
-            return state;   // TODO
-        case actionTypes.PUT_PHOTO:
-            return state;   // TODO
-        case actionTypes.GET_DEBATES:
-            return state;   // TODO
-        case actionTypes.POST_DEBATE:
-            return state;   // TODO
-        case actionTypes.GET_DEBATE:
-            return state;   // TODO
-        case actionTypes.GET_DEBATE_COMMENTS:
-            return state;   // TODO
-        case actionTypes.POST_DEBATE_COMMENT:
-            return state;   // TODO
+        // case actionTypes.PUT_DOCUMENT:
+        //     return state;   // TODO
+        // case actionTypes.POST_PHOTO:
+        //     return state;   // TODO
+        // case actionTypes.GET_PHOTO:
+        //     return state;   // TODO
+        // case actionTypes.PUT_PHOTO:
+        //     return state;   // TODO
+        // case actionTypes.GET_DEBATES:
+        //     return state;   // TODO
+        // case actionTypes.POST_DEBATE:
+        //     return state;   // TODO
+        // case actionTypes.GET_DEBATE:
+        //     return state;   // TODO
+        // case actionTypes.GET_DEBATE_COMMENTS:
+        //     return state;   // TODO
+        // case actionTypes.POST_DEBATE_COMMENT:
+        //     return state;   // TODO
         default:
             break;
     }
