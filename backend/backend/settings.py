77c2f597382edd6ac397a059c4bc72b0ebfc0d80
dkwanm1 <dkwanm00@gmail.com>
2019-11-02 15:29:38 +0900

    set csrftoken

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'user.User'
+
+CSRF_COOKIE_NAME = "csrftoken"
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,13 +8,19 @@ import { Provider } from 'react-redux';
 
 import store, { history } from './store/store';
 
+import axios from 'axios';
+
 import 'bootstrap/dist/css/bootstrap.css' // https://reactstrap.github.io/components/alerts/ 부트스트랩 머있는지
+
 
 ReactDOM.render(
     <Provider store={store}>
         <App history={history} />
     </Provider>,
     document.getElementById('root'));
+
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
