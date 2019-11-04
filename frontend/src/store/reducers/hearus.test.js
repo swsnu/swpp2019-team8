0c2bb52a17e517103e2e3ee904ed28c980739759
Donghyun Lee <dominic2009@snu.ac.kr>
2019-11-05 00:07:04 +0900

    Implement testing of GET_PETITION in reducers/hearus.test.js

---
+++ frontend/src/store/reducers/hearus.test.js
@@ -0,0 +1,22 @@
+import reducer from './hearus';
+import * as actionTypes from '../actions/actionTypes';
+
+describe('HearUs Reducer', () => {
+    it(`should return default state`, () => {
+        const newState = reducer(undefined, {});
+        expect(newState).toEqual({
+            selectedPetition: null,
+        });
+    });
+
+    it(`should get petition`, () => {
+        const stubSelectedPetition = { id: 1, title: 'title', content: 'content' };
+        const newState = reducer(undefined, {
+            type: actionTypes.GET_PETITION,
+            target: stubSelectedPetition,
+        });
+        expect(newState).toEqual({
+            selectedPetition: stubSelectedPetition,
+        });
+    });
+});
