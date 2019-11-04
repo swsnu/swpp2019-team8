30f95c91146edadd1a828717082c689773d137f8
Donghyun Lee <dominic2009@snu.ac.kr>
2019-11-05 00:01:29 +0900

    Add annotations in actions/index.js & actions/hearus.js

--- frontend/src/store/actions/hearus.js
+++ frontend/src/store/actions/hearus.js
@@ -1,44 +1,50 @@
 import * as actionTypes from './actionTypes';
 import axios from 'axios';
 
-export const postPetition_ = (/* TODO */) => {
+/*
+export const postPetition_ = (TODO) => {
     return {
         type: actionTypes.POST_PETITION,
         // TODO
     };
 };
 
-export const postPetition = (/* TODO */) => {
+export const postPetition = (TODO) => {
     return dispatch => {
         // TODO
     };
 };
+*/
 
-export const getPetitionsByVote_ = (/* TODO */) => {
+/*
+export const getPetitionsByVote_ = (TODO) => {
     return {
         type: actionTypes.GET_PETITIONS_BY_VOTE,
         // TODO
     };
 };
 
-export const getPetitionsByVote = (/* TODO */) => {
+export const getPetitionsByVote = (TODO) => {
     return dispatch => {
         // TODO
     };
 };
+*/
 
-export const getPetitionsByLatest_ = (/* TODO */) => {
+/*
+export const getPetitionsByLatest_ = (TODO) => {
     return {
         type: actionTypes.GET_PETITIONS_BY_LASTEST,
         // TODO
     };
 };
 
-export const getPetitionsByLatest = (/* TODO */) => {
+export const getPetitionsByLatest = (TODO) => {
     return dispatch => {
         // TODO
     };
 };
+*/
 
 export const getPetition_ = (petition) => {
     return {
@@ -54,43 +60,49 @@ export const getPetition = (petition_id) => {
     };
 };
 
-export const getMyPetitions_ = (/* TODO */) => {
+/*
+export const getMyPetitions_ = (TODO) => {
     return {
         type: actionTypes.GET_MY_PETITIONS,
         // TODO
     };
 };
 
-export const getMyPetitions = (/* TODO */) => {
+export const getMyPetitions = (TODO) => {
     return dispatch => {
         // TODO
     };
 };
+*/
 
-export const getPetitionComments_ = (/* TODO */) => {
+/*
+export const getPetitionComments_ = (TODO) => {
     return {
         type: actionTypes.GET_PETITION_COMMENTS,
         // TODO
     };
 };
 
-export const getPetitionComments = (/* TODO */) => {
+export const getPetitionComments = (TODO) => {
     return dispatch => {
         // TODO
     };
 };
+*/
 
-export const postPetitionComment_ = (/* TODO */) => {
+/*
+export const postPetitionComment_ = (TODO) => {
     return {
         type: actionTypes.POST_PETITION_COMMENT,
         // TODO
     };
 };
 
-export const postPetitionComment = (/* TODO */) => {
+export const postPetitionComment = (TODO) => {
     return dispatch => {
         // TODO
     };
 };
+*/
 
 // Statistic에 해당하는 것은 아직 안 만듦
