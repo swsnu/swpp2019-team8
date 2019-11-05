02646857fa422179badf8a6cdf53e093fc87e2ec
Donghyun Lee <dominic2009@snu.ac.kr>
2019-11-06 02:08:30 +0900

    Remove a line: import { push } from 'connected-react-router'

--- frontend/src/store/actions/user.js
+++ frontend/src/store/actions/user.js
@@ -1,8 +1,6 @@
 import * as actionTypes from './actionTypes';
 import axios from 'axios';
 
-import { push } from 'connected-react-router';
-
 export const postSignUp_ = () => {
     return {
         type: actionTypes.POST_SIGN_UP,
