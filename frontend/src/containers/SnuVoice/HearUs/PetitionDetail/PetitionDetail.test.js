064cea492714ae75a67db40516f3734b6a0fbcbb
Donghyun Lee <dominic2009@snu.ac.kr>
2019-11-06 10:06:22 +0900

    Revert "Add petitioner in PetitionDetail.js"

--- frontend/src/containers/SnuVoice/HearUs/PetitionDetail/PetitionDetail.test.js
+++ frontend/src/containers/SnuVoice/HearUs/PetitionDetail/PetitionDetail.test.js
@@ -16,11 +16,6 @@ const stubInitialState = {
         category: 'SELECTED_PETITION_TEST_CATEGORY',
         start_date: '0000-00-00T00:00:00.000',
         end_date: '0000-00-00T00:00:00.000',
-        author: 1,
-        link: 'SELECTED_PETITION_TEST_LINK',
-    },
-    selectedUser: {
-        nickname: 'SELECTED_PETITION_TEST_NICKNAME',
     },
 }
 
@@ -54,16 +49,12 @@ describe('<PetitionDetail />', () => {
         const wrapperCategory = component.find('.petitionsView_category');
         const wrapperStartDate = component.find('.petitionsView_start_date');
         const wrapperEndDate = component.find('.petitionsView_end_date');
-        const wrapperPetitioner = component.find('.petitionsView_petitioner');
-        const wrapperLink = component.find('.View_write_link');
         expect(wrapperTitle.at(0).text()).toBe('SELECTED_PETITION_TEST_TITLE');
         expect(wrapperContent.at(0).text()).toBe('SELECTED_PETITION_TEST_CONTENT');
         expect(wrapperVotes.at(0).text()).toBe('Votes: [ SELECTED_PETITION_TEST_VOTES ]');
         expect(wrapperCategory.at(0).text()).toBe('Category: SELECTED_PETITION_TEST_CATEGORY');
         expect(wrapperStartDate.at(0).text()).toBe('Start: 0000-00-00');
         expect(wrapperEndDate.at(0).text()).toBe('End: 0000-00-00');
-        expect(wrapperPetitioner.at(0).text()).toBe('Petitioner: SELECTED_PETITION_TEST_NICKNAME');
-        expect(wrapperLink.at(0).text()).toBe('Link 1 : SELECTED_PETITION_TEST_LINK');
     });
 
     it(`should not render SELECTED_PETITION`, () => {
@@ -83,15 +74,11 @@ describe('<PetitionDetail />', () => {
         const wrapperCategory = component.find('.petitionsView_category');
         const wrapperStartDate = component.find('.petitionsView_start_date');
         const wrapperEndDate = component.find('.petitionsView_end_date');
-        const wrapperPetitioner = component.find('.petitionsView_petitioner');
-        const wrapperLink = component.find('.View_write_link');
         expect(wrapperTitle.at(0).text()).toBe('');
         expect(wrapperContent.at(0).text()).toBe('');
         expect(wrapperVotes.at(0).text()).toBe('Votes: [  ]');
         expect(wrapperCategory.at(0).text()).toBe('Category: ');
         expect(wrapperStartDate.at(0).text()).toBe('Start: ');
         expect(wrapperEndDate.at(0).text()).toBe('End: ');
-        expect(wrapperPetitioner.at(0).text()).toBe('Petitioner: ');
-        expect(wrapperLink.at(0).text()).toBe('Link 1 : ');
     });
 });
