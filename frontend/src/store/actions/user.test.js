13b117970b6a31439c4c5376e01bb5b26ba749a0
dkwanm1 <dkwanm00@gmail.com>
2019-11-09 20:20:09 +0900

    modify testing for user reducers and actions

--- frontend/src/store/actions/user.test.js
+++ frontend/src/store/actions/user.test.js
@@ -12,12 +12,12 @@ describe('Axios User test', () => {
             studentId: '12'
         },
         verifyCode: '1',
-        signIn : true
+        signIn: true
     }
     const blank = {
         selectedUser: '',
         verifyCode: '',
-        signIn : false
+        signIn: false
     }
 
     let spyGetUser;
@@ -80,12 +80,10 @@ describe('Axios User test', () => {
                     resolve(reject);
                 })
             })
-        const alert = jest.spyOn(window, 'alert').mockImplementation(() => {})
         store.dispatch(actionCreators.postSignIn(stubUser)).then(() => {
             const newState = store.getState();
             expect(newState.usr.selectedUser).toStrictEqual('');
             expect(spy).toHaveBeenCalledTimes(1);
-            expect(alert).toHaveBeenCalledTimes(1);
             done();
         })
     })
@@ -149,6 +147,106 @@ describe('Axios User test', () => {
                 done();
             })
     });
+
+    it('checkDuplicate shoudl work', (done) => {
+        let spy = jest.spyOn(axios, 'get')
+            .mockImplementation((url) => {
+                return new Promise((resolve, reject) => {
+                    const result = {
+                        status: 200,
+                        data: {
+                            studentIdDuplicate: true
+                        }
+                    }
+                    resolve(result);
+                })
+            })
+        store.dispatch(actionCreators.checkStudentIdDuplicate('hi'))
+            .then(() => {
+                const newState = store.getState();
+                expect(newState.usr.studentIdDuplicate).toBe(true);
+                expect(spy).toHaveBeenCalledTimes(3)
+                done();
+            })
+
+        spy = jest.spyOn(axios, 'get')
+            .mockImplementation((url) => {
+                return new Promise((resolve, reject) => {
+                    const result = {
+                        status: 200,
+                        data: {
+                            nicknameDuplicate: true
+                        }
+                    }
+                    resolve(result);
+                })
+            })
+        store.dispatch(actionCreators.checkNicknameDuplicate('hi'))
+            .then(() => {
+                const newState = store.getState();
+                expect(newState.usr.nicknameDuplicate).toBe(true);
+                expect(spy).toHaveBeenCalledTimes(3)
+                done();
+            })
+
+        spy = jest.spyOn(axios, 'get')
+            .mockImplementation((url) => {
+                return new Promise((resolve, reject) => {
+                    const result = {
+                        status: 200,
+                        data: {
+                            emailDuplicate: true
+                        }
+                    }
+                    resolve(result);
+                })
+            })
+        store.dispatch(actionCreators.checkEmailDuplicate('hi'))
+            .then(() => {
+                const newState = store.getState();
+                expect(newState.usr.emailDuplicate).toBe(true);
+                expect(spy).toHaveBeenCalledTimes(3)
+                done();
+            })
+    })
+
+    it('should get verifyCode work', (done) => {
+        let spy = jest.spyOn(axios, 'get')
+            .mockImplementation((url) => {
+                return new Promise((resolve, rejcet) => {
+                    const result = {
+                        status: 200,
+                        data: {
+                            verifyCode: 123
+                        }
+                    }
+                    resolve(result);
+                })
+            })
+        store.dispatch(actionCreators.getVerifyCode('hi'))
+            .then(() => {
+                const newState = store.getState();
+                expect(newState.usr.verifyCode).toBe(123);
+                expect(spy).toHaveBeenCalledTimes(2)
+                done();
+            })
+
+        spy = jest.spyOn(axios, 'get')
+            .mockImplementation((url) => {
+                return new Promise((resolve, reject) => {
+                    resolve(reject);
+                })
+            })
+        store.dispatch(actionCreators.getVerifyCode('hi'))
+            .then(() => {
+                const newState = store.getState();
+                expect(newState.usr.verifyCode).toBe('');
+                expect(spy).toHaveBeenCalledTimes(2)
+                done();
+            })
+    })
+
+
 
 
 })
--- frontend/src/store/reducers/user.test.js
+++ frontend/src/store/reducers/user.test.js
@@ -2,89 +2,157 @@ import React from 'react'
 import userReducer from './user'
 import * as actionTypes from '../actions/actionTypes'
 
-describe ('user reducer', () => {
-    afterEach(() => {jest.clearAllMocks()})
-    it ('should signUp', () => {
+describe('user reducer', () => {
+
+    afterEach(() => { jest.clearAllMocks() })
+
+    it('should signUp', () => {
         const newState = userReducer(undefined, {
             type: actionTypes.POST_SIGN_UP
         })
         expect(newState).toEqual({
             selectedUser: '',
-            verifyCode : '',
-            signIn : false
+            verifyCode: '',
+            signIn: false
         })
     })
 
-    it ('should signIn', () => {
+    it('should signIn', () => {
         const newState = userReducer(undefined, {
             type: actionTypes.POST_SIGN_IN,
-            selectedUser : '1234',
-            signIn : true
+            selectedUser: '1234',
+            signIn: true
         })
         expect(newState).toEqual({
             selectedUser: '1234',
-            verifyCode : '',
-            signIn : true
+            verifyCode: '',
+            signIn: true
         })
     })
 
-    it ('should signOut', () => {
+    it('should signOut', () => {
         const beforState = {
-            selectedUser : '1234',
-            verifyCode : '123'
+            selectedUser: '1234',
+            verifyCode: '123'
         }
         const newState = userReducer(beforState, {
             type: actionTypes.GET_SIGN_OUT,
         })
         expect(newState).toEqual({
             selectedUser: '',
-            verifyCode : '',
-            signIn : false
+            verifyCode: '',
+            signIn: false
         })
     })
 
-    it ('should getUser not logged In', () => {
-        Storage.prototype.getItem = jest.fn(() => {return null})
+    it('should getUser not logged In', () => {
+        Storage.prototype.getItem = jest.fn(() => { return null })
         const newState = userReducer(undefined, {
             type: actionTypes.GET_USER,
             selectedUser: '123',
-            verifyCode : '12'
+            verifyCode: '12',
+            emailDuplicate: false,
+            nicknameDuplicate: false,
+            studentIdDuplicate: false
         })
         expect(newState).toEqual({
             selectedUser: '123',
-            verifyCode : '12',
-            signIn : false
+            verifyCode: '',
+            signIn: false,
+            emailDuplicate: false,
+            nicknameDuplicate: false,
+            studentIdDuplicate: false
         })
     })
 
-    
-    it ('should getUser not logged In', () => {
-        Storage.prototype.getItem = jest.fn(() => {return '1'})
+
+    it('should getUser not logged In', () => {
+        Storage.prototype.getItem = jest.fn(() => { return '1' })
         const newState = userReducer(undefined, {
             type: actionTypes.GET_USER,
             selectedUser: '123',
-            verifyCode : '12'
+            verifyCode: '12'
         })
         expect(newState).toEqual({
             selectedUser: '123',
-            verifyCode : '12',
-            signIn : true
+            verifyCode: '',
+            signIn: true,
+            emailDuplicate: false,
+            nicknameDuplicate: false,
+            studentIdDuplicate: false
         })
     })
 
-    it ('not allowed actionTypes', () => {
+    it('not allowed actionTypes', () => {
         const beforState = {
-            selectedUser : '1234',
-            verifyCode : '123',
-            signIn : false
+            selectedUser: '1234',
+            verifyCode: '123',
+            signIn: false
         }
         const newState = userReducer(beforState, {
             type: actionTypes.POST_PETITION,
         })
         expect(newState).toEqual({
             selectedUser: '1234',
-            verifyCode : '123',
-            signIn : false
+            verifyCode: '123',
+            signIn: false
         })
     })
+
+    it('should Check Duplicates works', () => {
+        let newState = userReducer(undefined, {
+            type: actionTypes.CHECK_EMAIL_DUPLICATE,
+            emailDuplicate: true,
+        })
+        expect(newState).toEqual({
+            selectedUser: '',
+            verifyCode: '',
+            signIn: false,
+            emailDuplicate: true,
+            nicknameDuplicate: false,
+            studentIdDuplicate: false
+        })
+
+        newState = userReducer(undefined, {
+            type: actionTypes.CHECK_NICKNAME_DUPLICATE,
+            nicknameDuplicate: true,
+        })
+        expect(newState).toEqual({
+            selectedUser: '',
+            verifyCode: '',
+            signIn: false,
+            emailDuplicate: false,
+            nicknameDuplicate: true,
+            studentIdDuplicate: false
+        })
+
+        newState = userReducer(undefined, {
+            type: actionTypes.CHECK_STUDENT_ID_DUPLICATE,
+            studentIdDuplicate: true,
+        })
+        expect(newState).toEqual({
+            selectedUser: '',
+            verifyCode: '',
+            signIn: false,
+            emailDuplicate: false,
+            nicknameDuplicate: false,
+            studentIdDuplicate: true
+        })
+
+    })
+
+    it('should getVerifyCode works', () => {
+        let newState = userReducer(undefined, {
+            type: actionTypes.GET_VERIFY_CODE,
+            verifyCode : 123,
+        })
+        expect(newState).toEqual({
+            selectedUser: '',
+            verifyCode : 123,
+            signIn : false,
+            emailDuplicate : false,
+            nicknameDuplicate : false,
+            studentIdDuplicate : false
+        })
+    } )
 })
