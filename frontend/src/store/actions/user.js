e7e73e004370957c3ba6d2cfc47aa100c790499b
dkwanm1 <dkwanm00@gmail.com>
2019-11-09 18:03:24 +0900

    Add feature for check duplicate

--- frontend/src/store/actions/user.js
+++ frontend/src/store/actions/user.js
@@ -9,30 +9,31 @@ export const postSignUp_ = () => {
 
 export const postSignUp = (user) => {
     return dispatch => {
-        return axios.post('/api/user/signup/', user).then(res => {
-            dispatch(postSignUp_());
-        });
+        return axios.post('/api/user/signup/', user)
+            .then(res => {
+                dispatch(postSignUp_());
+            });
     };
 };
 
 export const postSignIn_ = (response) => {
     return {
         type: actionTypes.POST_SIGN_IN,
-        selectedUser : response.selectedUser,
-        signIn : true
+        selectedUser: response.selectedUser,
+        signIn: true
     };
 };
 
 export const postSignIn = (user) => {
     return dispatch => {
         return axios.post('/api/user/signin/', user)
-        .then(res => {
-            dispatch(postSignIn_(res.data));
-        })
-        .catch(res => {
-            dispatch({type: actionTypes.POST_SIGN_IN, selectedUser : '', signIn : false})
-            window.alert('이메일 또는 비밀번호를 다시 학인해주십시오.')
-        });
+            .then(res => {
+                dispatch(postSignIn_(res.data));
+            })
+            .catch(res => {
+                dispatch({ type: actionTypes.POST_SIGN_IN, selectedUser: '', signIn: false })
+                window.alert('이메일 또는 비밀번호를 다시 학인해주십시오.')
+            });
     };
 };
 
@@ -44,49 +45,123 @@ export const getSignOut_ = () => {
 
 export const getSignOut = () => {
     return dispatch => {
-        return axios.get('/api/user/signout/').then(res => {
-            dispatch(getSignOut_());
-        })
+        return axios.get('/api/user/signout/')
+            .then(res => {
+                dispatch(getSignOut_());
+            })
     };
 };
 
-export const getUser_= (response) => {
+export const getUser_ = (response) => {
     return {
         type: actionTypes.GET_USER,
-        selectedUser: response.selectedUser,
-        verifyCode: response.verifyCode
+        selectedUser: response.selectedUser
     }
 };
 
 export const getUserByUserId = (userId) => {
     return dispatch => {
-        return axios.get('/api/user/userId/' + userId + '/').then(res => {
-            dispatch(getUser_(res.data))
-        });
+        return axios.get('/api/user/userId/' + userId + '/')
+            .then(res => {
+                dispatch(getUser_(res.data))
+            });
     }
 }
 
 export const getUserByEmail = (email) => {
     return dispatch => {
-        return axios.get('/api/user/email/' + email + '/').then(res => {
-            dispatch(getUser_(res.data));
-        });
+        return axios.get('/api/user/email/' + email + '/')
+            .then(res => {
+                dispatch(getUser_(res.data));
+            });
     };
 };
 
 export const getUserByStudentId = (studentId) => {
     return dispatch => {
-        return axios.get('/api/user/studentId/' + studentId + '/').then(res => {
-            dispatch(getUser_(res.data))
-        });
+        return axios.get('/api/user/studentId/' + studentId + '/')
+            .then(res => {
+                dispatch(getUser_(res.data))
+            });
     };
 };
 
 export const getUserByNickname = (nickname) => {
     return dispatch => {
-        return axios.get('/api/user/nickname/' + nickname + '/').then(res => {
-            dispatch(getUser_(res.data))
-        });
+        return axios.get('/api/user/nickname/' + nickname + '/')
+            .then(res => {
+                dispatch(getUser_(res.data))
+            });
         // TODO
     };
 };
+
+export const getVerifyCode_ = (response) => {
+    return {
+        type: actionTypes.GET_VERIFY_CODE,
+        verifyCode: response.verifyCode
+    }
+}
+
+export const getVerifyCode = (email) => {
+    return dispatch => {
+        return axios.get('/api/user/verifyCode/' + email + '/')
+            .then(res => {
+                dispatch(getVerifyCode_(res.data))
+            })
+            .catch(() => {
+                dispatch({
+                    type:actionTypes.GET_VERIFY_CODE,
+                    verifyCode : ''
+                })
+            })
+    }
+}
+
+export const checkEmailDuplicate_ = (response) => {
+    return {
+        type: actionTypes.CHECK_EMAIL_DUPLICATE,
+        emailDuplicate: response.emailDuplicate
+    }
+}
+
+export const checkEmailDuplicate = (email) => {
+    return dispatch => {
+        return axios.get('/api/user/check/email/' + email + '/')
+            .then(res => {
+                dispatch(checkEmailDuplicate_(res.data))
+            })
+    }
+}
+
+export const checkNicknameDuplicate_ = (response) => {
+    return {
+        type: actionTypes.CHECK_NICKNAME_DUPLICATE,
+        nicknameDuplicate: response.nicknameDuplicate
+    }
+}
+
+export const checkNicknameDuplicate = (nickname) => {
+    return dispatch => {
+        return axios.get('/api/user/check/nickname/' + nickname + '/')
+            .then(res => {
+                dispatch(checkNicknameDuplicate_(res.data))
+            })
+    }
+}
+
+export const checkStudentIdDuplicate_ = (response) => {
+    return {
+        type: actionTypes.CHECK_STUDENT_ID_DUPLICATE,
+        studentIdDuplicate: response.studentIdDuplicate
+    }
+}
+
+export const checkStudentIdDuplicate = (studentId) => {
+    return dispatch => {
+        return axios.get('/api/user/check/studentId/' + studentId + '/')
+            .then(res => {
+                dispatch(checkStudentIdDuplicate_(res.data))
+            })
+    }
+}
