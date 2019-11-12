ce11f33b2988c2e5dca7049c8268d169e278bd9a
rocky <m.seo1999@gmail.com>
2019-11-12 15:59:29 +0900

    Minor bug fix + changed layout of Debatelist

--- frontend/src/containers/SnuVoice/TellMe/DocumentDetail/DebateList/DebateList.js
+++ frontend/src/containers/SnuVoice/TellMe/DocumentDetail/DebateList/DebateList.js
@@ -4,23 +4,29 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { Button } from 'reactstrap';
 
-import Debate from '../../../../../components/Debate/debate'
+import Debate from '../../../../../components/Debate/debate';
+import Upperbar, { mapDispatchToProps } from '../../../UpperBar/UpperBar';
+import { mapStateToProps } from '../DocumentDetail';
 
 class DebateList extends Component {
 
     onClickDetailButton = (event) => {
-        this.props.history.push('???')
+        this.props.history.push("/tell_me/documents/" + this.props.match.params.document_title + "/debates/" + this.props.match.params.debate_id);
     }
 
     onClickCreateButton = (event) => {
-        this.props.history.push('???')
+        this.props.history.push("/tell_me/documents/" + this.props.match.params.document_title + "/debates/create");
     }
 
 
     render() {
-        
+        let documentTitle = '';
+
         return (
             <div>
+            <Upperbar />
+            <div className="TopOfPage">
+
                 <div className="DebateList">
                     <h1>Debate List</h1>
                     <h3>Document Title</h3>
@@ -29,8 +35,9 @@ class DebateList extends Component {
                 </div>
                 <Button>NEW</Button>
             </div>
+            </div>
         )
     }
 }
 
-export default connect(null, null)(withRouter(DebateList));
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(DebateList));
