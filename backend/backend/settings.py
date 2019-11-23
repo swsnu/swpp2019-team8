fac8a5ad62b3e4e43dc261b962a40a5b8bfd4a6f
dkwanm1 <dkwanm00@gmail.com>
2019-11-23 15:24:37 +0900

    hi

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'snuvoice',
+        'USER': 'root',
+        'PASSWORD': 'rldhks0203!',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
