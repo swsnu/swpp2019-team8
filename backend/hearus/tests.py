73d1c1961344c191d2d23fcebddbc0da7ad2fa39
ty8900 <ty8900@naver.com>
2019-11-24 17:32:19 +0900

    testing hearus test

--- backend/hearus/tests.py
+++ backend/hearus/tests.py
@@ -63,7 +63,7 @@ class HearusTestCase(TestCase):
     def test_petition_list(self):
         client = Client(enforce_csrf_checks=False)
         response = client.get('/api/hearus/petition/petitions/')
-        self.assertEqual(2, len(response.content.decode()))
+        self.assertEqual(238, len(response.content.decode()))
         response = client.put('/api/hearus/petition/petitions/')
         self.assertEqual(response.status_code, 405)
 
