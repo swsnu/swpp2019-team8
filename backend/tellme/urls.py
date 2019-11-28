71d70560a775817d4bde05b53d5a3b2f4cc1fa5f
dkwanm1 <dkwanm00@gmail.com>
2019-11-29 02:31:45 +0900

    get photo by title

--- backend/tellme/urls.py
+++ backend/tellme/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('document/', views.document),
     path('document/<str:document_title>/', views.document_title),
     path('photo/', views.photo),
-    #path('photo/<str:photo_title>/', views.photo_title),
+    path('photo/<str:photo_title>/', views.photo_title),
     path('document/<str:document_title>/debate/', views.debates_by_document),
     path('document/<str:document_title>/debate/<int:debate_id>/', views.debate_get),
     path('debate/<int:debate_id>/', views.debate_comments),
