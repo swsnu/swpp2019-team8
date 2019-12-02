b49fe1985ff3a3d64bb9c06597575c79e84141e5
dkwanm1 <dkwanm00@gmail.com>
2019-12-02 20:54:56 +0900

    fix code smell

--- frontend/src/containers/SnuVoice/TellMe/DocumentDetail/DocumentDetail.js
+++ frontend/src/containers/SnuVoice/TellMe/DocumentDetail/DocumentDetail.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router';
 
 import * as actionCreators from '../../../../store/actions/index';
