abd6156ae88671da9471176d15cc434998b63217
Donghyun Lee <dominic2009@snu.ac.kr>
2019-12-10 13:43:56 +0900

    Implement kakao share

--- frontend/src/containers/SnuVoice/HearUs/PetitionDetail/PetitionDetail.js
+++ frontend/src/containers/SnuVoice/HearUs/PetitionDetail/PetitionDetail.js
@@ -10,7 +10,6 @@ import * as actionCreators from '../../../../store/actions/index';
 import UpperBar from '../../UpperBar/UpperBar';
 import './PetitionDetail.css';
 
-
 class PetitionDetail extends Component {
     state = {
         isSelected: false,
@@ -22,6 +21,33 @@ class PetitionDetail extends Component {
     componentDidMount = async () => {
         await this.props.onGetPetition(this.props.match.params.petition_url);
         await this.props.onGetPetitionComments(this.props.match.params.petition_url);
+
+        window.Kakao.init('41c0076be4855dfb7bac65638652b1f9');
+        window.Kakao.Link.createDefaultButton({
+            container: '#kakao-link-btn',
+            objectType: 'feed',
+            content: {
+                title: this.props.selectedPetition.title,
+                description: '서울대학교 청원',
+                imageUrl: 'https://user-images.githubusercontent.com/26313346/70496202-20d01f00-1b52-11ea-9c55-c8213ab33352.png',
+                link: {
+                    mobileWebUrl: window.location.href,
+                    webUrl: window.location.href,
+                }
+            },
+            social: {
+                likeCount: this.props.selectedPetition.votes,
+            },
+            buttons: [
+                {
+                    title: '자세히 보기',
+                    link: {
+                        mobileWebUrl: window.location.href,
+                        webUrl: window.location.href,
+                    }
+                },
+            ]
+        });
     }
 
     onClickCommentConfirmButton = async () => {
@@ -185,6 +211,9 @@ class PetitionDetail extends Component {
                             {links}
                             <input type="text" id="ShareUrl" value={window.location.href} />
                             <Button onClick={this.onClickCopyURL}>URL 복사</Button>
+                            <a id="kakao-link-btn">
+                                <img src="//developers.kakao.com/assets/img/about/logos/kakaolink/kakaolink_btn_small.png" />
+                            </a>
                         </div>
                         <hr />
                         <div className="petitionsView_statistic">
