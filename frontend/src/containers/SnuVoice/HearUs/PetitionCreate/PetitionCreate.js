e9fecb29f5bb6804d3087492cd769629eee67a20
Donghyun Lee <dominic2009@snu.ac.kr>
2019-12-13 18:31:21 +0900

    Update guide in PetitionCreate.js

--- frontend/src/containers/SnuVoice/HearUs/PetitionCreate/PetitionCreate.js
+++ frontend/src/containers/SnuVoice/HearUs/PetitionCreate/PetitionCreate.js
@@ -10,6 +10,7 @@ import UpperBar from '../../UpperBar/UpperBar';
 
 class PetitionCreate extends Component {
     state = {
+        agreeToTerms: false,
         petitionTitle: '',
         categoryList: [
             { value: '-', label: '- Select Category -' },
@@ -48,6 +49,10 @@ class PetitionCreate extends Component {
 
         }
         //check authentication
+    }
+
+    onClickAgreeToTermsCheckBox = () => {
+        this.setState({ agreeToTerms: !this.state.agreeToTerms });
     }
 
     onChangeCategorySelect = (event) => {
@@ -103,25 +108,25 @@ class PetitionCreate extends Component {
                             <h1 className="petition_create_title">Create New Petition</h1>
                             <br />
                             <div className="inputTop">
-                                <Card>
+                                <Card id="petition_terms_textarea">
                                     Hear Us 운영 원칙
                                     <br /><br />
-                                    - SNUVOICE는 사상과 표현의 다양성을 존중합니다.
+                                    SNUVOICE는 사상과 표현의 다양성을 존중합니다.
                                     동시에 타인의 권리를 침해하거나 명예를 훼손하는 내용은 제한합니다.
                                     방송통신심의위원회의 '정보통신에 관한 심의 규정', 한국인터넷자율정책기구의 '정책규정' 등을 기반으로 문제 게시물은 삭제될 수 있습니다.
                                     자극적이고 혐오스러운 내용, 비속어, 폭력적 내용, 특정 대상을 비하하거나 차별을 조장하는 내용, 개인정보 유출을 비롯해 타인의 권리를 침해하는 내용, 반복되는 내용, 허위사실 등은 삭제나 숨김 처리될 수 있습니다.
-                                    <br />
-                                    - 청원글 게시 후 24시간 동안 5명의 사전동의를 받은 청원에 한해 SNUVOICE 게시판에 공개됩니다.
-                                    <br />
-                                    - 한 번 작성된 청원은 수정 및 삭제가 불가능합니다.
-                                    최초 청원 취지와 다른 내용으로 변경되는 것을 방지하여 청원 참여자의 의견을 보호하기 위한 조치이니 신중하게 작성하여 주시기 바랍니다.
-                                    <br /><br />
-                                    청원 작성 요령 안내
-                                    <br /><br />
-                                    - 새 청원글 작성 시, 내용을 대표하는 제목 및 관련 분야를 선택하신 후 내용을 입력해주시면 됩니다.
-                                    <br />
-                                    - 청원 내용과 관련 있는 웹사이트 또는 영상 URL이 있다면 첨부해주시기 바랍니다.
                                 </Card>
+                                <div className="agree">
+                                    <FormGroup>
+                                        <Input type="checkbox" id="agree_to_terms_checkbox" checked={this.state.agreeToTerms}
+                                            onChange={() => this.onClickAgreeToTermsCheckBox()}></Input>
+                                    </FormGroup>Agree
+                                </div>
+                                <p>
+                                    - 청원글 게시 후 24시간 동안 4명의 사전동의를 받은 청원에 한해 SNUVOICE 게시판에 공개됩니다.
+                                    <br />
+                                    - 한 번 작성된 청원은 수정 및 삭제가 불가능합니다. 최초 청원 취지와 다른 내용으로 변경되는 것을 방지하여 청원 참여자의 의견을 보호하기 위한 조치이니 신중하게 작성하여 주시기 바랍니다.
+                                </p>
                                 <FormGroup>
                                     <Label>Title</Label>
                                     <Input type="text" id="petition_title_input" placeholder="title"
@@ -154,7 +159,7 @@ class PetitionCreate extends Component {
                                 </FormGroup>
                                 <ButtonGroup className="buttons">
                                     <Button type="button" id="petition_confirm_button"
-                                        onClick={this.onClickPetitionConfirmButton} disabled={!this.state.petitionTitle || !this.state.petitionContent || this.state.selectedCategory === '-'}>CONFIRM</Button>
+                                        onClick={this.onClickPetitionConfirmButton} disabled={!this.state.agreeToTerms || !this.state.petitionTitle || !this.state.petitionContent || this.state.selectedCategory === '-'}>CONFIRM</Button>
                                     <Button type="button" id="petition_cancel_button"
                                         onClick={this.onClickPetitionCancelButton}>CANCEL</Button>
                                 </ButtonGroup>
