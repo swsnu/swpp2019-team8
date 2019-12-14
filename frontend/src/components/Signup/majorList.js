f89836e98e642e6346527f3fd54c3d4e217c65b9
dkwanm1 <dkwanm1@snu.ac.kr>
2019-12-14 16:44:05 +0900

    minor changes

--- frontend/src/components/Signup/majorList.js
+++ frontend/src/components/Signup/majorList.js
@@ -1,21 +1,22 @@
 export const majorList = {
 	all: [{ value: "-", label: "-" }],
 	agricultureAndLifeSciences: [
+		{ value: "-", label: "-" },
+		{ value: 'agriculturalAndResource Economics', label: 'Agricultural and Resource Economics' },
+		{ value: 'animalScienceaAndBiotechnology', label: 'Animal Science and Biotechnology' },
+		{ value: 'appliedBiology', label: 'Applied Biology' },
+		{ value: 'appliedLifeChemistry', label: 'Applied Life Chemistry' },
+		{ value: 'biomaterialsEngineering', label: 'Biomaterials Engineering' },
+		{ value: 'biosystemsEngineering', label: 'Biosystems Engineering' },
 		{ value: 'cropScienceAndBiotechnology', label: 'Crop Science and Biotechnology' },
-		{ value: 'horticulturalScience', label: 'Horticultural Science' },
-		{ value: 'vocationalEducationAndWorkforceDevelopment', label: 'Vocational Education and Workforce Development' },
-		{ value: 'forestEnvironmentalScience', label: 'Forest Environmental Science' },
 		{ value: 'environmentalMaterialsScience', label: 'Environmental Materials Science' },
 		{ value: 'foodScienceAndBiotechnology', label: 'Food Science and Biotechnology' },
-		{ value: 'animalScienceaAndBiotechnology', label: 'Animal Science and Biotechnology' },
-		{ value: 'appliedLifeChemistry', label: 'Applied Life Chemistry' },
-		{ value: 'appliedBiology', label: 'Applied Biology' },
-		{ value: 'biosystemsEngineering', label: 'Biosystems Engineering' },
-		{ value: 'biomaterialsEngineering', label: 'Biomaterials Engineering' },
+		{ value: 'forestEnvironmentalScience', label: 'Forest Environmental Science' },
+		{ value: 'horticulturalScience', label: 'Horticultural Science' },
 		{ value: 'landscapeArchitecture', label: 'Landscape Architecture' },
+		{ value: 'regionalInformation', label: 'Regional Information' },
 		{ value: 'ruralSystemEngineering', label: 'Rural System Engineering' },
-		{ value: 'agriculturalAndResource Economics', label: 'Agricultural and Resource Economics' },
-		{ value: 'regionalInformation', label: 'Regional Information' }
+		{ value: 'vocationalEducationAndWorkforceDevelopment', label: 'Vocational Education and Workforce Development' },
 	],
 	businessAdministration: [
 		{ value: "-", label: "-" },
@@ -23,69 +24,45 @@ export const majorList = {
 	],
 	education: [
 		{ value: "-", label: "-" },
-		{ value: "education", label: "Education" },
+		{ value: "biologyEducation", label: "Biology Education" },
+		{ value: "chemistryEducation", label: "Chemistry Education" },
 		{
-			value: "koreanLanguageEducation",
-			label: "Korean Language Education"
+			value: "earthScienceEducation",
+			label: "Earth Science Education"
 		},
+		{ value: "education", label: "Education" },
 		{ value: "englishEducation", label: "English Education" },
+		{ value: "ethicsEducation", label: "Ethics Education" },
+		{
+			value: "frenchLanguageEducation",
+			label: "French Language Education"
+		},
+		{ value: "historyEducation", label: "History Education" },
+		{ value: "geographyEducation", label: "Geography Education" },
 		{
 			value: "germanLanguageEducation",
 			label: "German Language Education"
 		},
 		{
-			value: "frenchLanguageEducation",
-			label: "French Language Education"
+			value: "koreanLanguageEducation",
+			label: "Korean Language Education"
 		},
-		{
-			value: "socialStudiesEducation",
-			label: "Social Studies Education"
-		},
-		{ value: "historyEducation", label: "History Education" },
-		{ value: "geographyEducation", label: "Geography Education" },
-		{ value: "ethicsEducation", label: "Ethics Education" },
 		{
 			value: "mathematicsEducation",
 			label: "Mathematics Education"
 		},
+		{ value: "physicalEducation", label: "Physical Education" },
 		{ value: "physicsEducation", label: "Physics Education" },
-		{ value: "chemistryEducation", label: "Chemistry Education" },
-		{ value: "biologyEducation", label: "Biology Education" },
 		{
-			value: "earthScienceEducation",
-			label: "Earth Science Education"
+			value: "socialStudiesEducation",
+			label: "Social Studies Education"
 		},
-		{ value: "physicalEducation", label: "Physical Education" }
 	],
 	engineering: [
 		{ value: "-", label: "-" },
 		{
-			value: "civilAndEnvironmentalEngineering",
-			label: "Civil and Environmetnal Engineering"
-		},
-		{
-			value: "mechanicalEngineerinig",
-			label: "Mechanical Engineering"
-		},
-		{
 			value: "aerospaceEngineering",
 			label: "Aerospace Engineering"
-		},
-		{
-			value: "materialsScienceAndEngineering",
-			label: " Materials Science and Engineering"
-		},
-		{
-			value: "electricalAndComputerEngineering",
-			label: "Electrical and Computer Engineering"
-		},
-		{
-			value: "computerScienceAndEngineering",
-			label: "Computer Science and Engineering"
-		},
-		{
-			value: "Chemical and Biological Engineering",
-			label: "Chemical and Biological Engineering"
 		},
 		{ value: "architecture", label: "Architecture" },
 		{
@@ -93,31 +70,61 @@ export const majorList = {
 			label: "Architecture Engineering"
 		},
 		{
-			value: "industrialEngineering",
-			label: "Industrial Engineering"
+			value: "chemicalandBiologicalEngineering",
+			label: "Chemical and Biological Engineering"
+		},
+		{
+			value: "computerScienceAndEngineering",
+			label: "Computer Science and Engineering"
+		},
+		{
+			value: "civilAndEnvironmentalEngineering",
+			label: "Civil and Environmetnal Engineering"
+		},
+		{
+			value: "electricalAndComputerEngineering",
+			label: "Electrical and Computer Engineering"
 		},
 		{
 			value: "energyResourcesEngineering",
 			label: "Energy Resources Engineering"
 		},
-		{ value: "nuclearEngineering", label: "Nuclear Engineering" },
+		{
+			value: "industrialEngineering",
+			label: "Industrial Engineering"
+		},
+		{
+			value: "materialsScienceAndEngineering",
+			label: " Materials Science and Engineering"
+		},
+		{
+			value: "mechanicalEngineerinig",
+			label: "Mechanical Engineering"
+		},
 		{
 			value: "navalArchitectureAndOceanEngineering",
 			label: "Naval Architecture and Ocean Engineering"
-		}
+		},
+		{ value: "nuclearEngineering", label: "Nuclear Engineering" },
 	],
 	fineArts: [
 		{ value: "-", label: "-" },
+		{ value: "craftsAndDesign", label: "Crafts and Design" },
 		{ value: "orientalPainting", label: "Oriental Painting" },
 		{ value: "painting", label: "Painting" },
 		{ value: "sculpture", label: "Sculpture" },
-		{ value: "craftsAndDesign", label: "Crafts and Design" }
 	],
 	humanities: [
 		{ value: "-", label: "-" },
+		{ value: "aesthetics", label: "Aesthetics" },
 		{
-			value: "koreanLanguageAndLiterature",
-			label: "Korean Language and Literature"
+			value: "archaeologyAndArtHistory",
+			label: "Archaeology and Art History"
+		},
+		{ value: "asianHistory", label: "Asian History" },
+		{
+			value: "asianLanguagesAndCivilizations",
+			label: "Asian Languages and Civilizations"
 		},
 		{
 			value: "chineseLanguageAndLiterature",
@@ -136,28 +143,22 @@ export const majorList = {
 			label: "German Language and Literature"
 		},
 		{
-			value: "russianLanguageAndLiterature",
-			label: "Russian Language and Literature"
-		},
-		{
 			value: "hispanicLanguageAndLiterature",
 			label: "Hispanic Language and Literature"
 		},
-		{ value: "linguistics", label: "Linguistics" },
-		{
-			value: "asianLanguagesAndCivilizations",
-			label: "Asian Languages and Civilizations"
-		},
 		{ value: "koreanHistory", label: "Korean History" },
-		{ value: "asianHistory", label: "Asian History" },
-		{ value: "westernHistory", label: "Western History" },
 		{
-			value: "archaeologyAndArtHistory",
-			label: "Archaeology and Art History"
+			value: "koreanLanguageAndLiterature",
+			label: "Korean Language and Literature"
 		},
+		{ value: "linguistics", label: "Linguistics" },
 		{ value: "philosophy", label: "Philosophy" },
 		{ value: "religiousStudies", label: "Religious Studies" },
-		{ value: "aesthetics", label: "Aesthetics" }
+		{
+			value: "russianLanguageAndLiterature",
+			label: "Russian Language and Literature"
+		},
+		{ value: "westernHistory", label: "Western History" },
 	],
 	humanEcology: [
 		{ value: "-", label: "-" },
@@ -177,33 +178,33 @@ export const majorList = {
 	],
 	medicine: [
 		{ value: "-", label: "-" },
+		{ value: "medicine", label: "Medicine" },
 		{ value: "preliminaryMedicine", label: "Preliminary Medicine" },
-		{ value: "medicine", label: "Medicine" }
 	],
 	music: [
 		{ value: "-", label: "-" },
-		{ value: "vocalMusic", label: "Vocal Music" },
 		{ value: "composition", label: "Composition" },
 		{ value: "instrumentalMusic", label: "Instrumental Music" },
-		{ value: "koreanMusic", label: "Korean Music" }
+		{ value: "koreanMusic", label: "Korean Music" },
+		{ value: "vocalMusic", label: "Vocal Music" },
 	],
 	naturalSciences: [
 		{ value: "-", label: "-" },
+		{ value: "biologicalSciences", label: "Biological Sciences" },
+		{ value: "chemistry", label: "Chemistry" },
+		{
+			value: "earthAndEnvironmentalSciences",
+			label: "Earth and Environmental Sciences"
+		},
 		{
 			value: "mathematicalSciences",
 			label: "Mathematical Sciences"
 		},
-		{ value: "statistics", label: "Statistics" },
 		{
 			value: "physicsAndAstronomy",
 			label: "Physics and Astronomy"
 		},
-		{ value: "chemistry", label: "Chemistry" },
-		{ value: "biologicalSciences", label: "Biological Sciences" },
-		{
-			value: "earthAndEnvironmentalSciences",
-			label: "Earth and Environmental Sciences"
-		}
+		{ value: "statistics", label: "Statistics" },
 	],
 	nursing: [
 		{ value: "-", label: "-" },
@@ -211,22 +212,22 @@ export const majorList = {
 	],
 	socialSciences: [
 		{ value: "-", label: "-" },
+		{ value: "anthropology", label: "Anthropology" },
+		{ value: "communication", label: "Communication" },
+		{ value: "economics", label: "Economics" },
+		{ value: "geography", label: "Geography" },
 		{
 			value: "politicalScienceAndInternationalRelations",
 			label: "Political Science and International Relations"
 		},
-		{ value: "economics", label: "Economics" },
-		{ value: "sociology", label: "Sociology" },
-		{ value: "anthropology", label: "Anthropology" },
 		{ value: "psychology", label: "Psychology" },
-		{ value: "geography", label: "Geography" },
 		{ value: "socialWelfare", label: "Social Welfare" },
-		{ value: "communication", label: "Communication" }
+		{ value: "sociology", label: "Sociology" },
 	],
 	pharmacy: [
 		{ value: "-", label: "-" },
-		{ value: "pharmacy", label: "Pharmacy" },
 		{ value: "manufacturingPharmacy", label: "Manufacturing Pharmacy" },
+		{ value: "pharmacy", label: "Pharmacy" },
 	],
 	veterinaryMedicine: [
 		{ value: "-", label: "-" },
